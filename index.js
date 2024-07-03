a54d079d78a057d7a2b9de6a41a1ac91be860060
KlyngerDantas <32768249+KlyngerDantas@users.noreply.github.com>
2024-07-03 12:31:47 +0000

    End point Readl All implementado

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+
+// Lista de personagem
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+// Read Al - [gGET]
+app.get('/item', function (req, res) {
+  res.send(lista)
+})
 app.listen(3000)
